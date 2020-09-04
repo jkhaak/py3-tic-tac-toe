415b1a15f4dfe52749a2e03430a987cc61c8dbd6
Jani Haakana <jkhaak@utu.fi>
2020-09-04 09:47:39 +0300

    add logic for winning condition

--- src/core.py
+++ src/core.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .utils import concat, chop, drop_at
+from .utils import concat, chop, drop_at, transpose
 from .draw import show_board, cls, goto
 
 
@@ -39,13 +39,15 @@ def play():
 
 
 class Game():
+    board_size = 3
+
     def __init__(self, player, grid):
         self.player = player
         self.grid = grid
 
     @classmethod
     def empty(cls):
-        return Game(1, [0 for _ in range(9)])
+        return Game(1, [0 for _ in range(cls.board_size ** 2)])
 
     def _str_player(self, i):
         if i == 1:
@@ -60,7 +62,7 @@ class Game():
     def valid_move(self, i):
         return 1 <= i and i <= 9 and self.grid[i-1] == 0
 
-    def next(self):
+    def next_player(self):
         return 2 if self.player == 1 else 1
 
     def move(self, move):
@@ -68,16 +70,28 @@ class Game():
             return self
 
         init, tail = drop_at(move, self.grid)
-        new_grid = init + [self.player] + tail
+        new_grid = init + [self.next_player()] + tail
 
-        return Game(self.next(), new_grid)
+        return Game(self.next_player(), new_grid)
 
     def draw_board(self):
         grid = chop(3, [self._str_player(i) for i in self.grid])
         show_board(grid)
 
     def wins(self):
-        return False
+        def diag(xs):
+            return [xs[i][i] for i in range(0, 3)]
+
+        # check: row, column, diagonal
+        pl = self.player
+        grid = chop(3, self.grid)
+        rows = [[i == pl for i in row] for row in grid]
+        cols = [[i == pl for i in col] for col in transpose(grid)]
+
+        mirrored = [row[::-1] for row in grid]
+        diags = [[i == pl for i in diag(grid)], [i == pl for i in diag(mirrored)]]
+
+        return any([all(i) for i in rows + cols + diags])
 
     def full(self):
         return all([i != 0 for i in self.grid])
